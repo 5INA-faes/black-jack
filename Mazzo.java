1e5c8774069c57f4aa3420ff499ed800de139fe4
DENNIS.FAES <DENNIS.FAES@buonarroti.tn.it>
2025-10-24 14:19:08 +0200

    Sono bastardo

--- Mazzo.java
+++ Mazzo.java
@@ -2,28 +2,50 @@ package black.jack;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Mazzo {
-    private ArrayList<Carta> carte;
-    private static int i;
-
+    private List<Carta> carte;
+    
     public Mazzo() {
-        
+        carte = new ArrayList<>();
+        inizializzaMazzo();
     }
-    
-    public void mescola(){
-        Collections.shuffle(carte);
-    }
-    
-    public Carta pescaCarta(){
-        if(i<312){
-            return carte.get(i);
-        }else{
-            
+
+    private void inizializzaMazzo() {
+        String[] semi = {"Cuori", "Quadri", "Fiori", "Picche"};
+        String[] valori = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        for (String seme : semi) {
+            for (String valore : valori) {
+                int punteggio;
+
+                // Assegno punteggio in base al valore
+                if (valore.equals("J") || valore.equals("Q") || valore.equals("K")) {
+                    punteggio = 10;
+                } else if (valore.equals("A")) {
+                    punteggio = 11; // l’Asso può valere 1 o 11, ma inizialmente 11
+                } else {
+                    punteggio = Integer.parseInt(valore);
+                }
+
+                carte.add(new Carta(seme, valore, punteggio));
+            }
         }
     }
-    
-    public boolean isEmpty(){
-        
+
+    public void mescola() {
+        Collections.shuffle(carte);
+    }
+
+    public Carta pescaCarta() {
+        if (!isEmpty()) {
+            return carte.remove(0);
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return carte.isEmpty();
     }
 }
